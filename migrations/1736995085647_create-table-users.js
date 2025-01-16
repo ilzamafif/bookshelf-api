81f55870ee15857d1d2126f1d7ba9a80c6d5bd47
ilzamafif <ilzamafif27@gmail.com>
2025-01-16 10:06:04 +0700

    membuat migrasi tabel user

---
+++ migrations/1736995085647_create-table-users.js
@@ -0,0 +1,32 @@
+const { password } = require('pg/lib/defaults');
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  })
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
