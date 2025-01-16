08c4e6638faa29c05185801d8e4f3a3504122eb8
ilzamafif <ilzamafif27@gmail.com>
2025-01-16 19:02:58 +0700

    membuat plugin authentications

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
+// menginpoer dotnev dan menjalankan konfigurasi dotenv
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
+
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
-const ClientError = require('./exceptions/ClientError');
-require('dotenv').config();
+
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
+const ClientError = require('./exceptions/ClientError');
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
  
   const server = Hapi.server({
     port: process.env.PORT,
@@ -35,6 +47,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
